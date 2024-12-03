9d18710317b68ee4bdf7c33ab579d7e37755ced5
Dima Oshuev <mazafon64@gmail.com>
2024-12-03 17:43:20 +0300

    добавил простую реализацию компонента отображения списка отелей

---
+++ src/components/HotelList.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+
+const HotelList = () => {
+    const [hotels, setHotels] = useState(null); // Состояние для хранения информации о гостинице
+
+    useEffect(() => {
+        fetch('http://localhost:5246/api/Hotel/GetHotels') // URL GET метода API
+            .then(res => res.json()) // преобразую ответ в JSON (хотя зачем если он уже в JSON приходит)
+            .then(data => setHotels(data)) // устанавливаю полученные данные в состояние на момент выполнения
+            .catch(err => console.log(err)); // ловлю ошибку, если запрос не удался
+    }, [])
+
+    if (!hotels) {
+        return <div>Загрузка данных</div>
+    }
+
+    // вывожу коллекцию json объектов
+    return (
+        <div>
+            <h1>Список отелей</h1>
+            {hotels.map((hotel) => (
+                <div key={hotel.id}>
+                    <h2>{hotel.name}</h2>
+                    <p>Адрес: {hotel.address}</p>
+                    <p>Город: {hotel.city}</p>
+                    <p>Номер телефона: {hotel.phoneNumber}</p>
+                    <p>Почта: {hotel.email}</p>
+                    <p>Рейтинг: {hotel.rating}</p>
+                    <p>Описание: {hotel.description}</p>
+                    <hr/>
+                </div>
+            ))}
+        </div>
+    );
+}
+
+export default HotelList;
